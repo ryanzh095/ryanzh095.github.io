86e123cbd2628486802d001fff8d04bbadd6d518
ryanzh095 <ryan_095@foxmail.com>
2025-02-14 00:40:03 +0000

    Deploying to gh-pages from @ ryanzh095/ryanzh095.github.io@f90a8b011036e5c526f84ddade1113334728690c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-gallery",
           title: "Gallery",
-          description: "A growing collection of your cool projects.",
+          description: "Under Construction",
           section: "Navigation",
           handler: () => {
             window.location.href = "/gallery/";
@@ -45,21 +45,11 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
